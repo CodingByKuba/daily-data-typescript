5d1e36b4e6b81cba14cc56e89b8529f2ee0f46e0
Jakub Marczak <19kuba92@gmail.com>
2023-09-04 03:04:36 +0200

    Working adding products to shopping list

--- src/components/elements/ProductElement.tsx
+++ src/components/elements/ProductElement.tsx
@@ -13,6 +13,34 @@ const NoteElement = (props: ProductType) => {
   const { fetchCallback } = useFetchContext();
   const { userState, userDispatch } = useUserContext();
   const [deleteError, setDeleteError] = useState<string>("");
+  const [productCount, setProductCount] = useState<number>(0);
+
+  const handleAdd = () => {
+    if (
+      confirm('Czy chcesz dodać produkt "' + props.title + '" do listy zakupów')
+    ) {
+      setDeleteError("");
+      fetchCallback({
+        url: config.AX_ROUTE_SHOPPINGLIST,
+        method: "POST",
+        payload: {
+          username: userState.username,
+          token: userState.token,
+          id: props.id,
+          count: productCount,
+        },
+        successCallback: (response: any) => {
+          if (response.data.error) return setDeleteError(response.data.error);
+          userDispatch({
+            type: ReducerActions.SET_DATA,
+            payload: { shoppingList: response.data.shoppingList },
+          });
+          setProductCount(0);
+        },
+        errorCallback: (error: any) => setDeleteError(error.message),
+      });
+    }
+  };
 
   const handleDelete = () => {
     if (confirm('Czy chcesz usunąć produkt "' + props.title + '"?')) {
@@ -42,6 +70,27 @@ const NoteElement = (props: ProductType) => {
       <NavLink to={props.id}>{props.title}</NavLink>
       {deleteError && <InfoBox type="error" message={deleteError} />}
       <pre>Jednostka: {props.unit.toUpperCase()}</pre>
+      <div>
+        <button
+          onClick={() =>
+            setProductCount((prev) => (prev >= 1 ? prev - 1 : prev))
+          }
+        >
+          &nbsp;-&nbsp;
+        </button>
+        <input
+          className="input-short"
+          type="number"
+          step={1}
+          min={0}
+          value={productCount.toString()}
+          onChange={(e) => setProductCount(parseFloat(e.target.value))}
+        />
+        <button onClick={() => setProductCount((prev) => prev + 1)}>
+          &nbsp;+&nbsp;
+        </button>
+        &nbsp;<button onClick={handleAdd}>Dodaj</button>
+      </div>
       <span>Utworzono: {dateParser(props.createdAt)}</span>
       {props.updatedAt && props.updatedAt !== props.createdAt && (
         <span>Edytowano: {dateParser(props.updatedAt)}</span>
